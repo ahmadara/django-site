63905e506af3d9fbebe8da3fc98cad85a4cd95f5
ahmadara <ahmad.majlesara@gmail.com>
2020-06-30 16:32:05 +0430

    comit

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -1,4 +1,4 @@
-"""mysite URL Configuration
+"""locallibrary URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -18,4 +18,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('catalog/', include('catalog.urls'))
 ]
