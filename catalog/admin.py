d18ad14cc7fd47c289a7f7f3da12914205f71595
ahmadara <ahmad.majlesara@gmail.com>
2020-07-01 16:32:22 +0430

    version 1.5

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+@admin.register(models.BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+    
+    fieldsets = (
+        ('information', {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+@admin.register(models.Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
