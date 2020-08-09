63eb6460d43c7e42a83b2205f71e84ce37317443
ahmadara <ahmad.majlesara@gmail.com>
2020-08-09 15:13:30 +0430

    cinema 1

--- catalog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url 
-from . import views
-app_name = 'catalog'
-urlpatterns = [
-        url(r'^$',views.index.as_view(),name='index'),
-        url(r'^books/$', views.BookDetailView.as_view(), name='books'),
-        url(r'^book/(?P<pk>\d+)/$', views.BookDetailView.as_view(), name='book-detail'),       
-]
